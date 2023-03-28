e1cefff2c22bc6f93f077e3fca1e94acce5b9782
vellasamy <venkadeshl2022@gmail.com>
2023-03-28 20:20:44 +0530

    update

---
+++ nested sum-7.js
@@ -0,0 +1,33 @@
+
+					//sum-7 nested method//
+							const isObject = function(val) {
+							if (val === null) {
+								return false;
+							}
+							return typeof val === 'object';
+						}; 
+										const person = {
+									first_name: 'Monica',
+									last_name: 'Geller',
+									phone: '915-996-9739',
+									email: 'monica37@gmail.com',
+									address: {
+										street: {
+											street_num: '495',
+											street_name: 'Grove Street',
+										},
+										city: 'New York',
+										country: 'USA',
+									    },
+									   };
+										const nestedObject =function (obj) {
+								for (const key in obj) {
+									if (isObject(obj[key])) {
+										nestedObject(obj[key]);
+									} else {
+										console.log(`${key} => ${obj[key]}`);
+									}
+								}
+							};
+
+							nestedObject(person);
